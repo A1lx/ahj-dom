d35fd9efcf89cac382571811035cf65a196ed3fc
Aleksey Klimov <alx111wrk@gmail.com>
2023-06-06 20:54:09 +0300

    init

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ const field = document.querySelector('.field');
 
 const goblin = document.createElement('img');
 goblin.classList.add('goblin');
-goblin.src = '..\img\goblin.png';
+goblin.src = '..\src\img\goblin.png';
 goblin.alt = 'goblin face';
 
 field.appendChild(goblin);
