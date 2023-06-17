2bc60521b46b843878e3eb0a7b4cc64f93ebd1b7
Aleksey Klimov <alx111wrk@gmail.com>
2023-06-17 14:47:40 +0300

    доработка

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,22 @@
-const field = document.querySelector('.field');
+const fieldParts = [...document.querySelectorAll('.field-part')];
 
-const goblin = document.createElement('img');
-goblin.classList.add('goblin');
-goblin.src = '..\src\img\goblin.png';
-goblin.alt = 'goblin face';
+function paintGoblin() {
+  const activeFieldPart = document.querySelector('.active_field-part');
+  let prevIndex = null;
 
-field.appendChild(goblin);
+  if (activeFieldPart) {
+    activeFieldPart.classList.remove('active_field-part');
+    // eslint-disable-next-line
+    prevIndex = index;
+  }
+
+  let index = Math.floor(Math.random() * 16);
+
+  while (prevIndex === index) {
+    index = Math.floor(Math.random() * 16);
+  }
+
+  fieldParts[index].classList.add('active_field-part');
+}
+
+setInterval(paintGoblin, 1000);
